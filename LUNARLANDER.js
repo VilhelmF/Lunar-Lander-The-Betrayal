71c11658b5d5e0418bdd98f205ee9c7962fccb21
thorgeirs <sigurdson22@gmail.com>
2014-11-04 23:56:46 +0000

    Laga Audio.js og aðeins lífið einfaldara í preloadDone()

--- LUNARLANDER.js
+++ LUNARLANDER.js
@@ -170,7 +170,7 @@ function renderSimulation(ctx) {
 // PRELOAD STUFF
 // =============
 
-var g_images = {};
+var g_images = [];
 
 function requestPreloads() {
 
@@ -185,18 +185,26 @@ function requestPreloads() {
     imagesPreload(requiredImages, g_images, preloadDone);
 }
 
-var g_sprites = {};
+var g_sprites = [];
 
 
 function preloadDone() {
 	
+	
+	for(var image in g_images) {
+		g_sprites[image] = new Sprite(g_images[image]);
+	}
+	
+	/*
     g_sprites.ship  = new Sprite(g_images.ship );
     g_sprites.ship2 = new Sprite(g_images.ship2);
     g_sprites.rock  = new Sprite(g_images.rock );	
-
+	*/
+	
     g_sprites.bullet = new Sprite(g_images.ship);
     g_sprites.bullet.scale = 0.25;
-
+	
+	
     entityManager.init();
     createInitialShips();
 
